21c450675651d3d1f614923a7bce62722f627b2e
ShrutJainn <shrutjain488@gmail.com>
2024-06-04 21:22:40 +0530

    Added user routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { signupUser } from "../controllers/userController.js";
+import {
+  getUser,
+  loginUser,
+  signupUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.get("/:userId", getUser);
 
 export default router;
